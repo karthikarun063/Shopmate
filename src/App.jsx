6d2c8d8a440c1dc4b0dda08797f68450df7a2634
Arunkarthik <karthikarun063@gamil.com>
2025-05-08 10:17:17 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='App'>
+
+        <Router>
+          <Routes>
+            <Route path='/' />
+            <Route path='/cart' />
+          </Routes>
+        </Router>
+      </div>
+  )
+}
+
+export default App
