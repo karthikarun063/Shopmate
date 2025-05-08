996cf8b3f88a15a0bca3693107503dfc86078d1a
Arunkarthik <karthikarun063@gamil.com>
2025-05-08 15:05:56 +0530

    Completed the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './components/Navbar'
+import { Cart } from './pages/cart/cart'
+import { Shop } from './pages/shop/shop'
+import { Contact } from "./pages/contact";
+import { ShopContextProvider } from "./context/shop-context";
+
 
 function App() {
-
-  return (
-      <div className='App'>
-
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' />
-            <Route path='/cart' />
-          </Routes>
-        </Router>
+    return (
+      <div className="App">
+        <ShopContextProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Shop />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </Router>
+        </ShopContextProvider>
       </div>
-  )
-}
+    );
+  }
 
 export default App
